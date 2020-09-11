3fd5b55701f353888098fca0d52908f636dae9c6
Alvaro Saavedra <alvaro.saavedra@jalasoft.com>
2020-09-11 02:23:54 -0400

    add item to cart

--- app/src/main/java/com/example/storerunner/ui/items/ItemsFragment.kt
+++ app/src/main/java/com/example/storerunner/ui/items/ItemsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -11,13 +12,16 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.storerunner.R
 import com.example.storerunner.adapters.ItemAdapter
 import com.example.storerunner.adapters.ItemCategoryAdapter
+import com.example.storerunner.interfaces.ItemAdapterListener
 import com.example.storerunner.models.Item
+import com.example.storerunner.models.ItemToAddToCart
 import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_items.*
 
-class ItemsFragment : Fragment() {
+class ItemsFragment : Fragment(), ItemAdapterListener {
 
     private lateinit var itemsViewModel: ItemsViewModel
+    var categoryId: Number? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,6 +39,7 @@ class ItemsFragment : Fragment() {
         itemRecycler.layoutManager = LinearLayoutManager(context)
         itemRecycler.setHasFixedSize(true)
         arguments?.getInt("categoryId")?.let { lifecycle ->
+            categoryId = lifecycle
             itemsViewModel.getItemsByCategoryId(lifecycle)
                 .observe(viewLifecycleOwner, Observer {
                     initRecycler(it)
@@ -43,7 +48,33 @@ class ItemsFragment : Fragment() {
     }
 
     private fun initRecycler(mutableList: MutableList<Item>) {
-        itemRecycler.adapter = ItemAdapter(mutableList)
+        itemRecycler.adapter = ItemAdapter(mutableList, this)
         (itemRecycler.adapter as ItemAdapter).notifyDataSetChanged()
+    }
+
+    override fun onTapAddToCart(item: Item) {
+        val itemToAddToCart = ItemToAddToCart(
+            0,
+            item.description,
+            item.itemNumber,
+            1,
+            item.name,
+            item.posX,
+            item.posY,
+            item.position,
+            item.price,
+            item.item_categoryId,
+            item.webImage
+        )
+        itemsViewModel.addItemToCart(itemToAddToCart).observe(viewLifecycleOwner, Observer {
+            arguments?.getInt("categoryId")?.let { lifecycle ->
+                categoryId = lifecycle
+                itemsViewModel.getItemsByCategoryId(lifecycle)
+                    .observe(viewLifecycleOwner, Observer {
+                        initRecycler(it)
+                    })
+                Toast.makeText(context, "1 Item added to your Cart", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
--- app/src/main/java/com/example/storerunner/ui/items/ItemsViewModel.kt
+++ app/src/main/java/com/example/storerunner/ui/items/ItemsViewModel.kt
@@ -3,6 +3,7 @@ package com.example.storerunner.ui.items
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.example.storerunner.models.Item
+import com.example.storerunner.models.ItemToAddToCart
 import com.example.storerunner.repositories.StoreRepository
 
 class ItemsViewModel : ViewModel() {
@@ -13,4 +14,9 @@ class ItemsViewModel : ViewModel() {
     fun getItemsByCategoryId(categoryId: Int): LiveData<MutableList<Item>> {
         return mStoreRepository.getItemsByCategory(categoryId)
     }
+
+    fun addItemToCart(itemToAddToCart: ItemToAddToCart): LiveData<ItemToAddToCart> {
+        return mStoreRepository.addItemToCart(itemToAddToCart)
+    }
+
 }
