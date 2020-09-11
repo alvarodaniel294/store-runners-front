b37b6b69e75ee25e522418eb0c06f294170779fd
Diego Bastos Heredia <diego.bastos@jalasoft.com>
2020-09-11 04:26:32 -0400

    Added optimization to list

--- app/src/main/java/com/example/storerunner/ui/shoppingcart/ShoppingCartViewModel.kt
+++ app/src/main/java/com/example/storerunner/ui/shoppingcart/ShoppingCartViewModel.kt
@@ -5,19 +5,23 @@ import androidx.lifecycle.ViewModel
 import com.example.storerunner.models.ItemCart
 import com.example.storerunner.repositories.StoreRepository
 
-class ShoppingCartViewModel:ViewModel() {
+class ShoppingCartViewModel : ViewModel() {
     private var mStoreRepository: StoreRepository =
         StoreRepository.StoreRepositoryObject.getInstance()
 
-    fun getAllShoppingCarts():LiveData<MutableList<ItemCart>>{
+    fun getAllShoppingCarts(): LiveData<MutableList<ItemCart>> {
         return mStoreRepository.getAllShoppingCarts()
     }
 
-    fun updateItemCart(itemCart: ItemCart):LiveData<ItemCart>{
+    fun updateItemCart(itemCart: ItemCart): LiveData<ItemCart> {
         return mStoreRepository.updateShoppingCart(itemCart)
     }
 
-    fun deleteFromCart(itemId:Number):LiveData<Unit>{
+    fun deleteFromCart(itemId: Number): LiveData<Unit> {
         return mStoreRepository.deleteFromCart(itemId)
+    }
+
+    fun optimizeList(shoppingList: MutableList<ItemCart>): LiveData<MutableList<ItemCart>> {
+        return mStoreRepository.optimizeRoute(shoppingList)
     }
 }
